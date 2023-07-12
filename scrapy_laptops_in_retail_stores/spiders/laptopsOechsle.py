e83327d5164b5e22c292f0015dc17847266e7192
Moisés Jurado <moises003@gmail.com>
2023-07-12 14:38:50 -0500

    end splash

--- scrapy_laptops_in_retail_stores/spiders/laptopsOechsle.py
+++ scrapy_laptops_in_retail_stores/spiders/laptopsOechsle.py
@@ -3,25 +3,37 @@ from urllib.parse import urljoin
 from scrapy_splash import SplashRequest
 import pprint as pp
 
+
 class LaptopsoechsleSpider(scrapy.Spider):
     name = "laptopsOechsle"
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
 
     def start_requests(self):
         urls = [
             # "https://www.oechsle.pe/tecnologia/computo/laptops?page=1",
             "https://www.oechsle.pe/tecnologia/computo/laptops-gamers?page=1",
-            "https://www.oechsle.pe/tecnologia/computo/laptops-gamers?page=2",
-            "https://www.oechsle.pe/tecnologia/computo/laptops-gamers?page=3",
-            "https://www.oechsle.pe/tecnologia/computo/laptops-gamers?page=4",
-            "https://www.oechsle.pe/tecnologia/computo/laptops-gamers?page=5",
+            # "https://www.oechsle.pe/tecnologia/computo/laptops-gamers?page=2",
+            # "https://www.oechsle.pe/tecnologia/computo/laptops-gamers?page=3",
+            # "https://www.oechsle.pe/tecnologia/computo/laptops-gamers?page=4",
+            # "https://www.oechsle.pe/tecnologia/computo/laptops-gamers?page=5",
         ]
-        retorno = []
+
+        urls_pintar = []
         for url in urls:
-            retorno.append(
-                SplashRequest(url, callback=self.parse, args={'wait': 5})
-            )
-        pp.pprint(retorno)
-        return retorno
+            urls_pintar.append(
+                SplashRequest(url, callback=self.parse, args={'wait': 5}))
+        print('-----------------------------------------------------------')
+        print('')
+        print('')
+        print('')
+        print('urls_pintar-BEGIN')
+        pp.pprint(urls_pintar)
+        print('urls_pintar-END')
+        print('')
+        print('')
+        print('')
+        print('-----------------------------------------------------------')
+        return urls_pintar
 
     def parse(self, response):
 
@@ -38,5 +50,16 @@ class LaptopsoechsleSpider(scrapy.Spider):
         # yield from response.follow_all(css="a.page-link",
         # callback=self.parse)
         urls_to_follow = response.css('a.page-link').getall()
+        print('-----------------------------------------------------------')
+        print('')
+        print('')
+        print('')
+        print('urls_to_follow-BEGIN')
+        pp.pprint(urls_to_follow)
+        print('urls_to_follow-END')
+        print('')
+        print('')
+        print('')
+        print('-----------------------------------------------------------')
         for url in urls_to_follow:
             yield SplashRequest(url, self.parse, args={'wait': 5})
