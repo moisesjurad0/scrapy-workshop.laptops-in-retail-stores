e29fefecc4082dfc990a1fd02ff9529cdf387e88
Moisés Jurado <moises003@gmail.com>
2023-07-13 02:11:30 -0500

    added item Laptop

--- scrapy_laptops_in_retail_stores/items.py
+++ scrapy_laptops_in_retail_stores/items.py
@@ -4,9 +4,37 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from dataclasses import dataclass
+import attr
 
 
 class ScrapyLaptopsInRetailStoresItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+
+class Laptop(scrapy.Item):
+    origin = scrapy.Field()
+    name = scrapy.Field()
+    price_offered = scrapy.Field()
+    price_with_card = scrapy.Field()
+    price_regular = scrapy.Field()
+
+
+@dataclass
+class Laptop2:
+    origin: str
+    name: str
+    price_offered: str
+    price_with_card: str
+    price_regular: str
+
+
+@attr.s
+class Laptop3:
+    origin = attr.ib()
+    name = attr.ib()
+    price_offered = attr.ib()
+    price_with_card = attr.ib()
+    price_regular = attr.ib()
